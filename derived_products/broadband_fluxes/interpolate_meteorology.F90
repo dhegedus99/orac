1e16aa2eb5a8b131320010d5c298d5e788e266f0
Dora Hegedus <dora.hegedus@stfc.ac.uk>
2025-11-03 14:28:16 +0000

    Check if meteorology is missing before doing interpolation and bugsrad calculation (for regional meteorology input files)

--- derived_products/broadband_fluxes/interpolate_meteorology.F90
+++ derived_products/broadband_fluxes/interpolate_meteorology.F90
@@ -22,7 +22,8 @@ subroutine interpolate_meteorology(lon,lat,nlev,xdim,ydim, &
                                    ilon,ilat,iP,iT,iH,iQ,iO3)
 
    use interpol_m
-
+   use common_constants_m
+   
    implicit none
 
    ! Input arguments
@@ -36,7 +37,7 @@ subroutine interpolate_meteorology(lon,lat,nlev,xdim,ydim, &
 
    ! Local variables
    type(interpol_t) :: interp
-   logical :: Wrap
+   logical :: Wrap, OutOfBounds
    integer :: NLat,NLon ! # of lat/lon indices
    real(8) :: Lat0,LatN,Lon0,LonN,delta_Lat,inv_delta_Lat,delta_Lon,inv_delta_Lon ! inputs to bilinear
    real(8) :: MinLon,MaxLon
@@ -61,16 +62,32 @@ subroutine interpolate_meteorology(lon,lat,nlev,xdim,ydim, &
    ! Does the grid wrap around the international date-line?
    Wrap = MinLon <= -180. .and. MaxLon >=  180.
 
-   ! Compute bilinear interpolation coefficients common to all interpolations
-   call bilinear_coef_reg_reg(Lon0, inv_delta_Lon, &
-        NLon, Lat0, inv_delta_Lat, &
-        NLat, ilon, ilat, interp, &
-        Wrap)
+   ! Check if the point is outside the bounds (considering wrap)
+   if (Wrap) then
+       OutOfBounds = .false.  ! If wrapping, we always interpolate
+   else
+       OutOfBounds = ilon < MinLon .or. ilon > MaxLon .or. ilat < min(Lat0,LatN) .or. ilat > max(Lat0,LatN)
+   end if
 
-   call interp_field2(P, iP, interp)
-   call interp_field2(T, iT, interp)
-   call interp_field2(H, iH, interp)
-   call interp_field2(Q, iQ, interp)
-   call interp_field2(O3, iO3, interp)
+   if (OutOfBounds) then
+       ! Assign missing value if out of bounds
+       iP  = sreal_fill_value
+       iT  = sreal_fill_value
+       iH  = sreal_fill_value
+       iQ  = sreal_fill_value
+       iO3 = sreal_fill_value
+   else
+       ! Compute bilinear interpolation coefficients common to all interpolations
+       call bilinear_coef_reg_reg(Lon0, inv_delta_Lon, &
+            NLon, Lat0, inv_delta_Lat, &
+            NLat, ilon, ilat, interp, &
+            Wrap)
+
+       call interp_field2(P, iP, interp)
+       call interp_field2(T, iT, interp)
+       call interp_field2(H, iH, interp)
+       call interp_field2(Q, iQ, interp)
+       call interp_field2(O3, iO3, interp)
+   end if
 
 end subroutine interpolate_meteorology
